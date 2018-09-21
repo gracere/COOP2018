1eb610716e62a36b20354f133ad7cb5e7407410a
Grace Ritter <grace.r20@student.parishepiscopal.org>
2018-09-21 11:28:07 -0500

    Just Trying to commit and push again.

--- Chapter 3/smaple module.py
+++ Chapter 3/smaple module.py
@@ -13,8 +13,7 @@
 #
 # Program Description
 #
-#
-#
+# A program to calculate the value of some change in dollars
 #
 # Algorithm (pseudocode)
 #
@@ -24,15 +23,18 @@
 #
 
 # change.py
-# A program to calculate the value of some change in dollars
+
+
 def main():
- print("Change Counter")
- print()
- print("Please enter the count of each coin type.")
- quarters = int(input("Quarters: "))
- dimes = int(input("Dimes: "))
- nickels = int(input("Nickels: "))
- pennies = int(input("Pennies: "))
- total = quarters * .25 + dimes * .10 + nickels * .05 + pennies * .01
- print()
- print("The total value of your change is", total)
+    print("Change Counter")
+    print("Please enter the count of each coin type.")
+    quarters = int(input("Quarters: "))
+    dimes = int(input("Dimes: "))
+    nickels = int(input("Nickels: "))
+    pennies = int(input("Pennies: "))
+    total = quarters * .25 + dimes * .10 + nickels * .05 + pennies * .01
+    print()
+    print("The total value of your change is", total)
+
+
+main()
