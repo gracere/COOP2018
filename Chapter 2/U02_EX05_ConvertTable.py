c7997d9f7f23b0eb4ab8a98fc9b9b92aef12bcb1
Grace Ritter <grace.r20@student.parishepiscopal.org>
2018-10-03 10:32:01 -0500

    Fixed in class(10/3)

---
+++ Chapter 2/U02_EX05_ConvertTable.py
@@ -0,0 +1,31 @@
+# U02_EX05_ConvertTable.py
+#
+#  Author: Grace Ritter
+#  Course: Coding for OOP
+# Section: A2
+#    Date: 19 Sep 2018
+#     IDE: PyCharm
+#
+# Assignment Info
+#  Exercise: 5
+#    Source: Python Programming
+#   Chapter: 2
+#
+# Program Description
+#   This program converts temperature from Celsius to Fahrenheit.
+#
+# Algorithm (pseudocode)
+#   Print Program introduction
+#   Get °C from user and assign to celsius
+#   Calculate °F using 9/5 * °C + 32 and assign to Fahrenheit
+#   Print °F
+
+def main():
+ print("This program converts temperature to Celsius to Fahrenheit")
+ celsius = eval(input("What is the Celsius temperature? "))
+ fahrenheit = 9/5 * celsius + 32
+ print("The temperature is ", fahrenheit, " degrees Fahrenheit.")
+ print (fahrenheit , celsius)
+
+
+main()
