bf5f99bec599ba4d807474fa1782363245508734
Grace Ritter <grace.r20@student.parishepiscopal.org>
2018-10-09 15:30:11 -0500

    Fixed exercise 15(10/9)

--- Chapter 3/U03_Ex15_Appxofpi.py
+++ Chapter 3/U03_Ex15_Appxofpi.py
@@ -16,19 +16,28 @@
 # This program computes the approximation of pi
 #
 # Algorithm (pseudocode)
-#
-# import the math aprrox of pi, from directory
-# get the imput of the aproxamation
+# import math
+# Print a program description
+# get accuracy input from user
+# insert into pi equation(pi += ((4.0 * (-1)**i) / (2*i + 1)))
+# print the value of pi
+# print the approximation of pi
+# put value of pi and approximation of pi into difference equation
+# print the difference
 #
 import math
+
+
 def main():
+    print("This program approximations pi, and then calculates the difference from that and the actual value of pi.")
     pi = 0
-    accuracy = 10
+    accuracy = int("What is the accuracy of the approximation of pi?")
     for i in range(0, accuracy):
         pi += ((4.0 * (-1)**i) / (2*i + 1))
-    print("The value of pi:" , math.pi)
-    print("This is the approximation of pi:" , pi)
+    print("The value of pi:", math.pi)
+    print("This is the approximation of pi:", pi)
     difference = (math.pi - pi)
-    print("This is the difference between the approximation of pi and the actual value of pi:" , difference)
+    print("This is the difference between the approximation of pi and the actual value of pi:", difference)
+
 
 main()
