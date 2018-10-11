a5ba451fd2d01ccfb8a76a8faa5c97fcedd5ab6d
Grace Ritter <grace.r20@student.parishepiscopal.org>
2018-10-11 13:22:35 -0500

    Still in Progress (10/11)

--- Chapter 3/U03_Ex14_AvgofaSeriusof#'s.py
+++ Chapter 3/U03_Ex14_AvgofaSeriusof#'s.py
@@ -1,4 +1,4 @@
-# U03_Ex14_AvgofaSeriusof#'s.py
+# U03_Ex14_AvgOfASeries#'s.py
 #
 #  Author: Grace Ritter
 #  Course: Coding for OOP
@@ -21,40 +21,16 @@
 # collect the actual numbers that need to be averaged
 # inset numbers collected into formula to average them. (n1 + n2 + n3 + n4 ...) / amount of numbers averaged
 # Print the average
+
+
 def main():
     print("This program finds the average of a series of numbers.")
-    amount = int("How many numbers do you want to average?: (The limit of number to average is 10.) ")
-    while True:
-        if amount == 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9 or 10:
-            n1 = input("Please enter a number that you wish to be averaged: ")
-            n2 = input("Please enter another number that you wish to be averaged: (If no other number needs averaged, enter zero)")
-            n3 = input("Please enter another number that you wish to be averaged: (If no other number needs averaged, enter zero)")
-            n4 = input("Please enter another number that you wish to be averaged: (If no other number needs averaged, enter zero)")
-            n5 = input("Please enter another number that you wish to be averaged: (If no other number needs averaged, enter zero)")
-            n6 = input("Please enter another number that you wish to be averaged: (If no other number needs averaged, enter zero)")
-            n7 = input("Please enter another number that you wish to be averaged: (If no other number needs averaged, enter zero)")
-            n8 = input("Please enter another number that you wish to be averaged: (If no other number needs averaged, enter zero)")
-            n9 = input("Please enter another number that you wish to be averaged: (If no other number needs averaged, enter zero)")
-            n10 = input("Please enter another number that you wish to be averaged: (If no other number needs averaged, enter zero)")
-            if input == "zero":
-                break
-            average = int(n1) + int(n2) + int(n3) + int(n4) + int(n5) + int(n6) + int(n7) + int(n8) + int(n9) + int(n10) / amount
-            print ("The average of the numbers that you inputted: ", average)
-        if amount is not == 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9 or 10:
-            amount = int("Please enter an amount of numbers that you wish to be averaged less than or equal to 10:")
-            n1 = input("Please enter a number that you wish to be averaged: ")
-            n2 = input("Please enter another number that you wish to be averaged: (If no other number needs averaged, enter zero)")
-            n3 = input("Please enter another number that you wish to be averaged: (If no other number needs averaged, enter zero)")
-            n4 = input("Please enter another number that you wish to be averaged: (If no other number needs averaged, enter zero)")
-            n5 = input("Please enter another number that you wish to be averaged: (If no other number needs averaged, enter zero)")
-            n6 = input("Please enter another number that you wish to be averaged: (If no other number needs averaged, enter zero)")
-            n7 = input("Please enter another number that you wish to be averaged: (If no other number needs averaged, enter zero)")
-            n8 = input("Please enter another number that you wish to be averaged: (If no other number needs averaged, enter zero)")
-            n9 = input("Please enter another number that you wish to be averaged: (If no other number needs averaged, enter zero)")
-            n10 = input("Please enter another number that you wish to be averaged: (If no other number needs averaged, enter zero)")
-            if input == "zero":
-                break
-            average = int(n1) + int(n2) + int(n3) + int(n4) + int(n5) + int(n6) + int(n7) + int(n8) + int(n9) + int(n10) / amount
-            print("The average of the numbers that you inputted: ", average)
+    amount = int("How many numbers do you want to average?: ")
+    for i in range(amount):
+        f = input("Please enter another number that you wish to be averaged:")
+        f = int(f)
+    average = f / amount
+    print("The average of the numbers that you inputted: ", average)
+
 
 main()
