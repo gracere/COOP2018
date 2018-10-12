1b1583a568609a02d91319b06693b0d07c70eaeb
Grace Ritter <grace.r20@student.parishepiscopal.org>
2018-10-12 10:53:54 -0500

    Fixed in class (10/12)

--- Chapter 3/U03_Ex03_MolecularlbsofCarb.py
+++ Chapter 3/U03_Ex03_MolecularlbsofCarb.py
@@ -1,4 +1,4 @@
-# U03_Ex03_MolecularlbsofCarb.py
+# U03_Ex03_MolecularWeightOfCarb.py
 #
 #  Author: Grace Ritter
 #  Course: Coding for OOP
@@ -16,16 +16,18 @@
 # This program computes the pounds of a carb
 #
 # Algorithm (pseudocode)
-#
-# Input number of carbon, hydrogen and oxygen atoms
-# Find total weights of each type of atom
-#
+# Print program description
+# Get Input number of carbon, hydrogen and oxygen atoms from user
+# Find total weights of each type of atom, and the weight of them combined together
+
+
 def main():
-    print("This program finds the total molecular weight of a combination of hydrogen, carbon and oxygen atoms (Try H2O)")
+    print("This program finds the total molecular weight of a combination of hydrogen, carbon and oxygen atoms.")
     h = eval(input("How many hydrogen atoms? "))
     c = eval(input("Carbon? "))
     o = eval(input("Oxygen? "))
     total = (h * 1.00794) + (c * 12.0107) + (o * 15.9994)
-    print("The total molecular weight is", total)
+    print("The total molecular weight is:", total, "grams per mole.", o, "Oxygen", h, "Hydrogen, and", c, "Carbon.")
+
 
 main()
