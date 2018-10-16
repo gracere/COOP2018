622cf8a68e135c7a76d1c3d7fdde8892e00c9edc
Grace Ritter <grace.r20@student.parishepiscopal.org>
2018-10-16 11:17:35 -0500

    Fixed code (10/14)

--- Chapter 3/U03_Ex07_DistanceFormula.py
+++ Chapter 3/U03_Ex07_DistanceFormula.py
@@ -16,16 +16,28 @@
 # This program calculates the distance formula
 #
 # Algorithm (pseudocode)
-#
-# (x1 + x2 / 2) + (y1 + y2 / 2))
-# insert distance formula into code, input list
-#
-import math # math needed for sqrt
+# Print program description
+# Get data points from user
+# separate data and find parts of data, then put together
+# use formula to do that: (x1 + x2 / 2) + (y1 + y2 / 2))
+# insert distance formula into code
+# print findings
 
-# distance function
-def dist(p1, p2):
-    return math.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
 
-# run through input and reorder in [(x, y), (x,y) ...] format
-input = ["9.5 7.5", "10.2 19.1", "9.7 10.2"] # original input list (entered by spacing the two points)
-points = [map(float, point.split()) for point in input] # final list
+def main():
+    x1 = input('Please enter a value for x1')
+    x2 = input('Please enter a value for x2')
+    y1 = input('Please enter a value for y1')
+    y2 = input('Please enter a value for y2')
+    print(x1, x2, y1, y2)
+    xcords = x2 - x1
+    print('Difference of X Co-Ordinates:', xcords)
+    ycords = y2 - y1
+    print('Difference of Y Co-Ordinates:', ycords)
+    sumofx = xcords / 2
+    sumofy = ycords / 2
+    distform = sumofx + sumofy
+    print("Distance of points:", distform)
+
+
+main()
