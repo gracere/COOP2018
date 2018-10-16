20db050cc5a8502ca016fb9018a8b59413d8eda0
Grace Ritter <grace.r20@student.parishepiscopal.org>
2018-10-16 11:22:49 -0500

    Fixed code (10/14)

--- Chapter 3/U03_Ex09_areaofTriw-sides.py
+++ Chapter 3/U03_Ex09_areaofTriw-sides.py
@@ -16,17 +16,20 @@
 # Python Program to find the area of triangle
 #
 # Algorithm (pseudocode)
-#
-# find user number
-# input into equation
-# print out input
-#
+# Print program description
+# find user numbers for all three sides
+# input user numbers into equation: s = (a + b + c) / 2
+# convert the sides into the area: area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+# print out input of area
+
+
 def main():
     a = float(input('Enter first side: '))
     b = float(input('Enter second side: '))
     c = float(input('Enter third side: '))
     s = (a + b + c) / 2
     area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
-    print('The area of the triangle is %0.2f' %area)
+    print('The area of the triangle is %0.2f:' %area)
+
 
 main()
