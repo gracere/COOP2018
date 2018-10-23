d9af4762d28dfc6f98e33a861cd879eb2cf9493e
Grace Ritter <grace.r20@student.parishepiscopal.org>
2018-10-23 07:47:10 -0500

    Just started working with objects, and had to watch MANY tutorials to just understand what was going on.  (10/23)

---
+++ Chapter 4/U04_Ex01_SquarestoCircles.py
@@ -0,0 +1,48 @@
+# U04_Ex01_SquaresToCircles.py
+#
+#  Author: Grace Ritter
+#  Course: Coding for OOP
+# Section: A2
+#    Date: 17 Oct 2018
+#     IDE: PyCharm
+#
+# Assignment Info
+#  Exercise: 1
+#    Source: Python Programming
+#   Chapter: 4
+#
+# Program Description
+#
+# This program draws squares into circles
+#
+# Algorithm (pseudocode)
+# print program description
+# import graphics
+# create a graph
+# get the parameters of the square
+
+import turtle
+
+
+def draw_square(some_turtle):
+    for i in range(1, 5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+
+
+def draw_art():
+    window = turtle.Screen()
+    window.bgcolor('red')
+    grace = turtle.Turtle()
+    grace.color('yellow')
+    grace.shape('turtle')
+    grace.speed(2)
+
+    for i in range(1, 36):
+        draw_square(grace)
+        grace.right(10)
+
+    window.exitonclick()
+
+
+draw_art()
