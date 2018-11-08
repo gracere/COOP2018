6dbee8626e4e422d1f3ecb21db2c7b2d97cde819
Grace Ritter <grace.r20@student.parishepiscopal.org>
2018-11-08 11:33:58 -0600

    Fixed in Class (11/8)

--- Chapter 4/U04_Ex01_SquarestoCircles.py
+++ Chapter 4/U04_Ex01_SquarestoCircles.py
@@ -20,27 +20,41 @@
 # import graphics
 # create a graph
 # get the parameters of the square
-
-import turtle
-
-
-def draw_square(some_turtle):
-    for i in range(1, 5):
-        some_turtle.forward(100)
-        some_turtle.right(90)
+from graphics import *
 
 
-def draw_art():
-    window = turtle.Screen()
-    window.bgcolor('blue')
-    grace = turtle.Turtle()
-    grace.color('black')
-    grace.shape('arrow')
-    grace.speed(2)
+def main():
+    win = GraphWin("Square Draw", 1280, 720)
 
-    for i in range(1, 36):
-        draw_square(grace)
-        grace.right(10)
+    intro = Text(Point(600, 300),"This program allows you to place 10 30x30 red squares."
+                                 "\ndraw each square by clicking on the window."
+                                 "\nTo remove this message left click on the window.")
+    intro.setSize(25)
+    intro.draw(win)
+    win.getMouse()
+    intro.undraw()
+
+    p1 = Point(0, 0)
+    p2 = Point(p1.getX() - 30, p1.getY() - 30)
+    square = Rectangle(p1, p2)
+    square.setOutline("red")
+    square.setFill("red")
+    square.draw(win)
+
+    for i in range(10):
+        p = win.getMouse()
+        center = square.getCenter()
+        dx = p.getX() - center.getX()
+        dy = p.getY() - center.getY()
+        clone = square.clone()
+        clone.move(dx, dy)
+        clone.draw(win)
+
+    conc = Text(Point(600, 300), "Click again to quit")
+    conc.setSize(25)
+    conc.draw(win)
+    win.getMouse()
+    win.close()
 
 
-draw_art()
+main()
