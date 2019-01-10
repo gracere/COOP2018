75681d58c5186498b6deb9241b59198bf39ec0b1
Grace Ritter <grace.r20@student.parishepiscopal.org>
2019-01-10 10:53:49 -0600

    Fixed some bugs 1/10

--- Chapter 5/U05_Ex4_Acronym.py
+++ Chapter 5/U05_Ex4_Acronym.py
@@ -23,19 +23,17 @@
 # split them from the string
 # And Get the first Letter from each word, and put them together
 # print letters, and a description of the information given.
-import string
 
 
 def main():
     print("This program generates acronyms.")
     print("I will ask you to Enter a phrase, and I will give you its acronym.")
-    phrase = int("Enter a phrase : ")
-    words = string.split(phrase)
+    phrase = input("Enter a phrase : ")
+    words = phrase.split()
     acronym = ""
-    for w in words:
-        acronym = acronym + w[0]
-    acronym = string.upper(acronym)
-    print("The acronym is : ", acronym)
+    for letter in words:
+        acronym = acronym + letter[0]
+    print("The acronym is: {0} from the phrase:".format(acronym), phrase)
 
 
 main()
