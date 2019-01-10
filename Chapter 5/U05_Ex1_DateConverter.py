1b6674f965dc737b12b8fe71ad5bd0021b90febe
Grace Ritter <grace.r20@student.parishepiscopal.org>
2019-01-10 11:22:05 -0600

    Fixed some bugs 1/10

--- Chapter 5/U05_Ex1_DateConverter.py
+++ Chapter 5/U05_Ex1_DateConverter.py
@@ -28,14 +28,18 @@
 
 
 def main():
-    print("This program will take your birthday and then it will convert it into a nice form")
+    print("This program will take your birthday and then it will convert it.")
     date = input("Enter a date mm/dd/yyyy ")
     month, day, year = date.split("/")
     print("You where born on", month, "/", day, "/", year)
     months = ("January", "February", "March", "April", "May", "June", "July",
               "August", "September", "October", "November", "December")
     month = months[int(month)-1]
-    print("The converted date is", month, day, year)
+    days = ("1st,", "2nd,", "3rd,", "4th,", "5th,", "6th,", "7th,", "8th,", "9th,", "10th,", "11th,", "12th,", "13th,",
+            "14th,", "15th,", "16th,", "17th,", "18th,", "19th,", "20th,", "21st,", "22nd,", "23rd,", "24th,", "25th,",
+            "26th,", "27th,", "28th,", "29th,", "30th,", "31st,")
+    day = days[int(day)-1]
+    print("The converted date is {0}".format(month), day, year)
 
 
 main()
